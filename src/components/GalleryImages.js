3440514bb3637dbc8c6eb02eef06ec4128b0567a
ashish <128252821+aashishjoshi5@users.noreply.github.com>
2024-10-12 12:29:38 +0530

    updated gallery

--- src/components/GalleryImages.js
+++ src/components/GalleryImages.js
@@ -7,12 +7,22 @@ import g6 from '../assets/gallery/6.jpg'
 import g7 from '../assets/gallery/7.jpg'
 import g8 from '../assets/gallery/8.jpg'
 import g9 from '../assets/gallery/9.jpg'
-import g10 from '../assets/gallery/10.jpg'
+// import g10 from '../assets/gallery/10.jpg'
+import g11 from '../assets/gallery/11.jpg'
+import g12 from '../assets/gallery/12.jpg'
+import g13 from '../assets/gallery/13.jpg'
+import g14 from '../assets/gallery/14.jpg'
+import g15 from '../assets/gallery/15.jpg'
+import g16 from '../assets/gallery/16.jpg'
+import g17 from '../assets/gallery/17.jpg'
+import g18 from '../assets/gallery/18.jpg'
+import g19 from '../assets/gallery/19.jpg'
 
 
 const GalleryImages=[
+    g17,
     g1,
-    g2,g3,g4,g5,g6,g7,g8,g9,g10
+    g2,g3,g4,g5,g6,g7,g8,g9,g11,g12,g13,g14,g15,g16,g18,g19
     
     
 
