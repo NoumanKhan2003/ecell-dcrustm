b65890e086537d2a324f1ef03821c9e0aebd9b08
ashish <128252821+aashishjoshi5@users.noreply.github.com>
2024-10-14 21:26:01 +0530

    text update

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -42,14 +42,14 @@ const Homepage = () => {
       <div  className=" flex justify-center flex-wrap lg:flex  lg:w-full h-auto md:h-[700px] " >
         <div  className=" w-[90%]  text-center md:text-left md:w-1/2 pt-10  ">
             <h1    className="  text-[#000000] font-semibold tracking-tighter text-[2.5rem] md:text-6xl pb-10   md:pb-14"> E-Cell DCRUSTM</h1>
-            <h2  className="  text-gray-800 text-3xl md:text-5xl pb-14">Accelerate Your <br />
+            <h2  className="  text-gray-800 text-3xl md:text-5xl md:pb-14 pb-5">Accelerate Your <br />
              <span className="text-blue-600" >{text}</span> <span className="text-blue-600"><Cursor cursorStyle="|" /></span>
               <br />Journey!</h2>
-            <p  className="text-xl  md:text-xl text-gray-800 pb-14 mb-8  md:pr-14">Welcome to the DCRUST Murthal entrepreneurship community, where visionary ideas meet collaboration, fueling the next generation of groundbreaking innovations!</p>
+            <p  className="  md:text-xl text-gray-800  md:pb-14 mb-8  md:pr-14 px-6 md:pl-0">Welcome to the DCRUST Murthal entrepreneurship community, where visionary ideas meet collaboration, fueling the next generation of groundbreaking innovations!</p>
            {/* <button className=" bg-[#0065fc]  text-xl text-white py-5 px-10 rounded-md hover:bg-[#144c8b] hover:shadow-lg"><Link className="" to='/about'>Get Started!</Link><FaArrowRight className="inline ml-2 items-center" /></button> */}
            <Link 
          to="/about" 
-         className="bg-[#0065fc] text-xl text-white py-5 px-10 rounded-md hover:bg-[#144c8b] hover:shadow-lg flex items-center justify-center md:justify-start w-fit mx-auto md:mx-0"
+         className="bg-[#0065fc]   md:text-xl text-white py-5 mb-4 px-10 rounded-md hover:bg-[#144c8b] hover:shadow-lg flex items-center justify-center md:justify-start w-fit mx-auto md:mx-0"
           
           >
          Get Started!
