06e60b6f085c4440eaa703eb4cb2bdf2175207f9
Nouman Khan <noumankhan16859@gmail.com>
2025-02-23 17:26:43 +0530

    basic database schema created for admin

---
+++ server/models/adminModel.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+//admin schema created
+const adminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+//Models created
+const admin = mongoose.model("admin", adminSchema);
+export default admin;
--- server/server.js
+++ server/server.js
@@ -1,15 +1,21 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import model from "./models/adminModel.js";
+
 const app = express();
 dotenv.config();
-mongoose.connect(process.env.Url).then(() => {
+
+mongoose
+  .connect(process.env.Url)
+  .then(() => {
     console.log("Database connected");
-}).catch((error) => {
+  })
+  .catch((error) => {
     console.log("Error connecting to database", error);
-});
+  });
 
 app.get("/", (req, res) => {
-res.send("Hello World!");
+  res.send("Hello World!");
 });
 app.listen(process.env.Port || 3000);
