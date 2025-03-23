0d255d995eedc69447606aeff4d7467e1ad675a5
Nouman Khan <noumankhan16859@gmail.com>
2025-03-23 12:22:51 +0530

    Refactor EventsPage styling: change background color to white and replace div with Material-UI Box for image container

--- client/src/pages/EventsPage.jsx
+++ client/src/pages/EventsPage.jsx
@@ -192,16 +192,16 @@ const EventsPage = () => {
               .map((data) => (
                 <div
                   key={data._id}
-                  className="bg-red-200 group relative mx-auto overflow-hidden rounded-xl"
+                  className="bg-white group relative mx-auto overflow-hidden rounded-xl"
                 >
-                  <div className="w-auto h-[350px]">
+                  <Box sx={{width:"auto",height:340}}>
                     <img
                       src={data.image}
                       alt="event-img"
                       loading="lazy"
                       className="object-cover group-hover:rotate-3 group-hover:scale-105 transition-transform"
                     />
-                  </div>
+                  </Box>
                   <div className="from-transparent via-transparent to-black/50 bg-gradient-to-b absolute inset-0 group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
                     <div className="absolute inset-0 px-9 flex flex-col items-center justify-center translate-y-[90%] group-hover:translate-y-0 transition-all">
                       <h1 className="text-white font-bold text-3xl pb-3 text-center">
