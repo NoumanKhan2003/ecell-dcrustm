a89eaf0d83b6f41d96eec0db603fcc7347c91572
Nouman Khan <noumankhan16859@gmail.com>
2025-03-23 13:31:03 +0530

    Enhance EventsPage image handling: adjust Box properties for better responsiveness and update image class for improved display

--- client/src/pages/EventsPage.jsx
+++ client/src/pages/EventsPage.jsx
@@ -194,16 +194,16 @@ const EventsPage = () => {
                   key={data._id}
                   className="bg-white group relative mx-auto overflow-hidden rounded-xl"
                 >
-                  <Box sx={{width:"auto",height:340}}>
+                  <Box sx={{width:"auto",maxHeight:350,overflow:"hidden"}}>
                     <img
                       src={data.image}
                       alt="event-img"
                       loading="lazy"
-                      className="object-cover group-hover:rotate-3 group-hover:scale-105 transition-transform"
+                      className=" object-contain group-hover:rotate-3 group-hover:scale-105 transition-transform"
                     />
                   </Box>
                   <div className="from-transparent via-transparent to-black/50 bg-gradient-to-b absolute inset-0 group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70">
-                    <div className="absolute inset-0 px-9 flex flex-col items-center justify-center translate-y-[90%] group-hover:translate-y-0 transition-all">
+                    <div className="absolute inset-0 px-9 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 translate-y-[90%] group-hover:translate-y-0 transition-all">
                       <h1 className="text-white font-bold text-3xl pb-3 text-center">
                         {data.title}
                       </h1>
