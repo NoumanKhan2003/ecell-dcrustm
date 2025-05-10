742c9937676266ad612ae393889bb6c946031c31
Nouman Khan <noumankhan16859@gmail.com>
2025-05-10 17:47:12 +0530

    Loader added in login page

--- client/src/pages/UserLoginPage.jsx
+++ client/src/pages/UserLoginPage.jsx
@@ -11,11 +11,13 @@ import {
   IconButton,
   InputAdornment,
 } from "@mui/material";
+import LoaderComponent from "../components/Loader.jsx";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
 
 const UserLoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
+  const [loading, setloading] = useState(false);
   const [loginInfo, setLoginInfo] = useState({
     email: "",
     password: "",
@@ -41,6 +43,7 @@ const UserLoginPage = () => {
     if (!email || !password) {
       return handleError("All fields are required");
     }
+    setloading(true);
     try {
       const url = `${import.meta.env.VITE_BACKEND_URL}/user/userLogin`;
       const response = await fetch(url, {
@@ -71,6 +74,8 @@ const UserLoginPage = () => {
       }
     } catch (err) {
       handleError(err.message || "Something went wrong");
+    } finally {
+      setloading(false);
     }
   };
 
@@ -79,74 +84,80 @@ const UserLoginPage = () => {
   }, []);
 
   return (
-    <Container
-      sx={{
-        marginTop: 0,
-        marginBottom: "4rem",
-        height: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Paper
-        elevation={3}
-        sx={{ padding: 3, maxWidth: 350, width: "100%", borderRadius: 2 }}
-      >
-        <Typography variant="h6" align="center" fontWeight={600}>
-          Sign in to E-Cell(DCRUSTM)
-        </Typography>
-        <Box component="form" sx={{ mt: 2 }} onSubmit={handleSignin}>
-          <TextField
-            fullWidth
-            name="email"
-            label="Enter username"
-            margin="normal"
-            variant="outlined"
-            onChange={handleChange}
-            value={loginInfo.email}
-          />
-          <TextField
-            fullWidth
-            name="password"
-            label="Enter password"
-            type={showPassword ? "text" : "password"}
-            margin="normal"
-            variant="outlined"
-            onChange={handleChange}
-            value={loginInfo.password}
-            slotProps={{
-              input: {
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <IconButton onClick={togglePassword} edge="end">
-                      {showPassword ? (
-                        <VisibilityOffIcon />
-                      ) : (
-                        <VisibilityIcon />
-                      )}
-                    </IconButton>
-                  </InputAdornment>
-                ),
-              },
-            }}
-          />
-          <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            sx={{ mt: 2 }}
-            type="submit"
+    <>
+      {loading ? (
+        <LoaderComponent />
+      ) : (
+        <Container
+          sx={{
+            marginTop: 0,
+            marginBottom: "4rem",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Paper
+            elevation={3}
+            sx={{ padding: 3, maxWidth: 350, width: "100%", borderRadius: 2 }}
           >
-            Sign in
-          </Button>
-        </Box>
-        <Typography variant="body2" align="center" sx={{ mt: 2 }}>
-          No account / Forgot Password ?<br /> Contact Admin for username and
-          password
-        </Typography>
-      </Paper>
-    </Container>
+            <Typography variant="h6" align="center" fontWeight={600}>
+              Sign in to E-Cell(DCRUSTM)
+            </Typography>
+            <Box component="form" sx={{ mt: 2 }} onSubmit={handleSignin}>
+              <TextField
+                fullWidth
+                name="email"
+                label="Enter username"
+                margin="normal"
+                variant="outlined"
+                onChange={handleChange}
+                value={loginInfo.email}
+              />
+              <TextField
+                fullWidth
+                name="password"
+                label="Enter password"
+                type={showPassword ? "text" : "password"}
+                margin="normal"
+                variant="outlined"
+                onChange={handleChange}
+                value={loginInfo.password}
+                slotProps={{
+                  input: {
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton onClick={togglePassword} edge="end">
+                          {showPassword ? (
+                            <VisibilityOffIcon />
+                          ) : (
+                            <VisibilityIcon />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  },
+                }}
+              />
+              <Button
+                fullWidth
+                variant="contained"
+                color="primary"
+                sx={{ mt: 2 }}
+                type="submit"
+              >
+                Sign in
+              </Button>
+            </Box>
+            <Typography variant="body2" align="center" sx={{ mt: 2 }}>
+              No account / Forgot Password ?<br /> Contact Admin for username
+              and password
+            </Typography>
+          </Paper>
+        </Container>
+      )}
+    </>
   );
 };
 
